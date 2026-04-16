44c4c30218175cf77cf01cf41684d33626405d0b
vivekrawat1998 <128046112+vivekrawat1998@users.noreply.github.com>
2026-04-16 15:03:24 +0530

    this is the final comit

--- config/server.js
+++ config/server.js
@@ -1,10 +1,14 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // url: 'http://13.205:121:242',
+
+  // ✅ ADD THIS
+  url: 'https://sanseed.org',
+
   app: {
     keys: env.array('APP_KEYS'),
   },
+
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
